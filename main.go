52893ab08f935cac6ed18d189a0f193501298da4
Waqar Amin <vacaramin86@gmail.com>
2023-07-04 06:06:47 +0500

    server setup tested

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 	setupApi()
+	log.Fatal(http.ListenAndServe(":3080", nil))
 }
 func setupApi() {
 	http.Handle("/", http.FileServer(http.Dir("./frontend")))
