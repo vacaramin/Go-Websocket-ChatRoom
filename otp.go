f35ae2a17f997dbc18e2a3b52f1acb89209218e8
Waqar Amin <vacaramin86@gmail.com>
2023-07-07 14:58:45 +0500

    OTP file completed

--- otp.go
+++ otp.go
@@ -21,5 +21,31 @@ func (rm RetentionMap) NewOTP() OTP {
 		Key:     uuid.NewString(),
 		Created: time.Now(),
 	}
+	rm[o.Key] = o
 	return o
+}
+
+func (rm RetentionMap) VerifyOTP(otp string) bool {
+	if _, ok := rm[otp]; !ok {
+		return false
+	}
+	delete(rm, otp)
+	return true
+}
+
+func (rm RetentionMap) Retention(ctx context.Context, retentionPeriod time.Duration) {
+	ticker := time.NewTicker(400 * time.Millisecond)
+	for {
+		select {
+		case <-ticker.C:
+			for _, otp := range rm {
+				if otp.Created.Add(retentionPeriod).Before(time.Now()) {
+					delete(rm, otp.Key)
+				}
+			}
+		case <-ctx.Done():
+			return
+		}
+	}
+
 }
