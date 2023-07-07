e9cbe6269158f925063cb778b12c798627e15f05
Waqar Amin <vacaramin86@gmail.com>
2023-07-07 10:54:21 +0500

    Cross site origin bug fixed

--- manager.go
+++ manager.go
@@ -82,7 +82,7 @@ func (m *Manager) removeClient(client *Client) {
 func checkOrigin(r *http.Request) bool {
 	origin := r.Header.Get("Origin")
 	switch origin {
-	case "https://localhost:8080":
+	case "http://localhost:8080":
 		return true
 	default:
 		return false
