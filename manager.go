92a3f0cbfaf8dd28434bdcfac14401f7b9d7fc1e
Waqar Amin <vacaramin86@gmail.com>
2023-07-09 01:55:44 +0500

    Otp implemented in the backend

--- manager.go
+++ manager.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -21,13 +23,15 @@ var (
 type Manager struct {
 	client ClientList
 	sync.RWMutex
+	otps     RetentionMap
 	handlers map[string]EventHandler
 }
 
-func NewManager() *Manager {
+func NewManager(ctx context.Context) *Manager {
 	return &Manager{
 		client:   make(ClientList),
 		handlers: make(map[string]EventHandler),
+		otps:     NewRetentionMap(ctx, 5*time.Second),
 	}
 }
 
